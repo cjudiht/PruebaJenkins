5ba5c98d2a2871db6f1901539c61913d8bb4f60c
CJudith <cluny02@gmail.com>
2018-03-04 01:20:09 -0500

    ok

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CJUDITH
+ */
 public class HelloWorld {
 
+   
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
-
+    
 }
